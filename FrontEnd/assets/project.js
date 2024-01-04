a18e4e490ae25448eb773d848d2bf8bed38ec15a
Intissar TOUCHENT <touchentintissar@gmail.com>
2024-01-04 18:55:23 +0100

    Ajout du tri des projets par catégorie

--- FrontEnd/assets/project.js
+++ FrontEnd/assets/project.js
@@ -1,15 +1,17 @@
 //Récupération des projets de l'API
-const reponse = await fetch ('http://localhost:5678/api/works')
-const projects = await reponse.json()
+const projects = await fetch ('http://localhost:5678/api/works').then(projects => projects.json())
 
 //Générer les projets 
 function generateProjects (projects){
     for(let i=0; i<projects.length; i++){
         const article = projects[i]
+        
         // Récupération de l'élément du DOM qui accueillera les projets
         const divGallery = document.querySelector(".gallery")
+       
         // Création d’une balise dédiée à un projet
         const projectElement = document.createElement("article")
+       
         // Création des autres balises 
         const imageElement = document.createElement("img")
         imageElement.src = article.imageUrl
@@ -23,5 +25,46 @@ function generateProjects (projects){
 }
 }
 
+// Suppression des éléments HTML
 document.querySelector(".gallery").innerHTML = ""
+
+// Appel de la fonction qui génère les projets à partir de l'API
 generateProjects(projects)
+
+//Créer le filtre Objets
+const filterObjects = document.querySelector(".objects")
+filterObjects.addEventListener("click", function(){
+    const filteredElement = projects.filter(function(project){
+        return project.category.name === "Objets"
+    })
+    document.querySelector(".gallery").innerHTML=""
+    generateProjects(filteredElement)    
+})
+
+//Créer le filtre Appartements
+const filterFlats = document.querySelector(".flats")
+filterFlats.addEventListener("click", function(){
+    const filteredElement = projects.filter(function(project){
+        return project.category.name === "Appartements"
+    })
+    document.querySelector(".gallery").innerHTML=""
+    generateProjects(filteredElement)    
+})
+
+//Créer le filtre Hôtels et restaurants
+const filterHotels = document.querySelector(".hotels")
+filterHotels.addEventListener("click", function(){
+    const filteredElement = projects.filter(function(project){
+        return project.category.name === "Hotels & restaurants"
+    })
+    document.querySelector(".gallery").innerHTML=""
+    generateProjects(filteredElement)    
+})
+
+//Créer le filtre tous
+const filterAll = document.querySelector(".all")
+filterAll.addEventListener("click", function(){
+    document.querySelector(".gallery").innerHTML=""
+    generateProjects(projects)    
+})
+
